ef534c7fc83ee86bd53df23619d4648d61e825fa
yizik <syizikm@gmail.com>
2026-05-18 12:42:25 +0300

    render.com yml

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -3,4 +3,15 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom', 'react-router-dom'],
+          signals: ['@preact/signals-react'],
+          highlighter: ['react-syntax-highlighter'],
+        },
+      },
+    },
+  },
 })
